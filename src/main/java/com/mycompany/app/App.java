643611f0411f0d6fe20db903d7d1bdf6f4240e23
muskansahni <muskansahni2018@gmail.com>
2022-08-30 11:37:55 +0000

    hello world 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,16 +8,12 @@ public class App
 
    
 
-    public App() {}
+
 
     public static void main(String[] args) {
         System.out.println("Hello world"); 
 System.out.println("Hello world");
 System.out.println("Hello world");
-    }
-
-    private final String getMessage() {
-        return message;
     }
 
 }
